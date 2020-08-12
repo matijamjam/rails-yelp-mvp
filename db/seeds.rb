4d14127af98a1bb479a235b0b9c5159fa12137e6
Mathilda Djamdjian <mathilda.djamdjian@gmail.com>
2020-08-12 21:10:59 +0200

    crud

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,19 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create([( name: 'Star Wars' }, ( name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+restaurant = Restaurant.create( name: 'restaurant1', address: 'address 1', category: 'french')
+puts "Creating restaurants..."
+restaurant1 = Restaurant.create( name: 'restaurant2', address: 'address 2', category: 'belgian')
+puts "Creating restaurants..."
+restaurant2 = Restaurant.create( name: 'restaurant3', address: 'address 3', category: 'japanese')
+puts "Creating restaurants..."
+restaurant3 = Restaurant.create( name: 'restaurant4', address: 'address 4', category: 'chinese')
+puts "Creating restaurants..."
+restaurant4 = Restaurant.create( name: 'restaurant5', address: 'address 5', category: 'italian')
